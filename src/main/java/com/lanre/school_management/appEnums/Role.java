2ae6f0ad748c20f938ab887a620196e042a94ef4
larry659 <larryfunmbi26@gmail.com>
2023-08-08 12:31:36 +0100

    creation of packages

---
+++ src/main/java/com/lanre/school_management/appEnums/Role.java
@@ -0,0 +1,5 @@
+package com.lanre.school_management.appEnums;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/lanre/school_management/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.lanre.school_management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}
